52502229682629bc5ce2011466b4f0a3ac46a289
Nicholas Murphy <nicholas.murphy@student.fairfield.edu>
2025-02-17 22:59:30 -0500

    Initial Linked List Tester

--- linkedList/LinkedListTester.java
+++ linkedList/LinkedListTester.java
@@ -3,12 +3,36 @@ package linkedList;
 public class LinkedListTester {
 
 	public static void main(String[] args) {
-		// create implementation, then...
-		
 		LinkedList tester = new LinkedListImpl();
-		tester.addItem("an item to add");
-		
 
+		// Add items
+		tester.addItem("first item");
+		tester.addItem("second item");
+		tester.addItem("third item");
+
+		// List items
+		tester.listItems();
+
+		// Check item count
+		System.out.println("Item count: " + tester.itemCount());
+
+		// Check if an item is in the list
+		System.out.println("Is 'second item' in list? " + tester.isItemInList("second item"));
+
+		// Delete an item
+		tester.deleteItem("second item");
+		tester.listItems();
+
+		// Insert before
+		tester.insertBefore("new item", "third item");
+		tester.listItems();
+
+		// Insert after
+		tester.insertAfter("another new item", "first item");
+		tester.listItems();
+
+		// Sort items
+		tester.sort();
+		tester.listItems();
 	}
-
 }
